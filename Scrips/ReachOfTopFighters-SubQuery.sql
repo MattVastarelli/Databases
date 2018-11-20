721a8ffd94e09e65e238817e7291cd0b9043a12f
Matthew Vastarelli <mvast1@unh.newhaven.edu>
2018-11-19 21:26:14 -0500

    rename and 7th query

--- Scrips/ReachOfTopFighters-SubQuery.sql
+++ Scrips/ReachOfTopFighters-SubQuery.sql
@@ -3,15 +3,21 @@
 USE CombatSports
 
 SELECT
-	TopFighters.*
+	TopFighters.fighterName,
+	TopFighters.TotalWins,
+	p.reach
 FROM
 	(
 		SELECT TOP 5
+			f.fighterID,
 			f.fighterName,
 			COUNT(p.isWinner) TotalWins
 		FROM Panles p
 		INNER JOIN Fighters f
 			ON p.fighterID = f.fighterID
-		GROUP BY f.fighterName
+		GROUP BY f.fighterName, f.fighterID
 		ORDER BY TotalWins DESC
 	) TopFighters
+INNER JOIN PhysicalStats p
+	ON TopFighters.fighterID = p.fighterID
+WHERE p.normalWeight = '155'
